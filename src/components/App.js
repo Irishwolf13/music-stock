d373b88ba59fe6f927d0460e93060742481e9f13
John Rooney <rooneyjohn@gmail.com>
2023-01-22 14:39:44 -0500

    small change to app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom"; // Going with new Routes instead of old "Switch"
 import Home from "./Home.js"
 import Search from "./Search.js"
 import Portfolio from "./Portfolio.js"
