09521653a9d219af5af1e05fcae6a390588e49b8
John Rooney <rooneyjohn@gmail.com>
2023-01-24 10:16:52 -0500

    changed db.json

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,18 @@
+import Card from './Card.js'
 
-function Home() {
-    
+function Home({ rawData }) {
+    const displayItems = rawData.map(item => (
+      <Card
+        key={item.id} 
+        genre={item.genre}
+        image_url={item.image_url}
+      />
+    ))
     
 
   return(
-    <div>
-      <title> Home </title>
+    <div className="container">
+      {displayItems}
     </div>
   )
 }
