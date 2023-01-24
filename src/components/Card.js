fdb137bbd88ae6f8d277ec8093d68161826807b4
John Rooney <rooneyjohn@gmail.com>
2023-01-24 18:18:55 -0500

    Got some sound working

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,29 @@
-function Card({artistName, trackName, image_url, id, handleArtistClicked}) {
+function Card({preview, artistName, trackName, image_url, id, handleArtistClicked}) {
+  const myPreview = new Audio(preview)
+  let playStop = "Play"
+  let isPlaying = false
+
+  const handlePlayMusic = () => {
+    if (isPlaying === false) {
+      myPreview.play()
+      isPlaying = true
+      playStop = "Stop"
+    }else {
+      myPreview.pause()
+      isPlaying = false
+      playStop = "Play"
+    }
+  }
 
   return(
     <>
       <div className="cardContainer">
         <button onClick={(e) => handleArtistClicked(id)} className="cardButton">Buy</button>
+        <button onClick={handlePlayMusic}>{ playStop } </button>
         <img className="coverArt" src={image_url} ></img>
         <div className="artistName">{artistName}</div>
         <div className="albumName">{trackName}</div>
+        <div>frank</div>
       </div>
     </>
   )
